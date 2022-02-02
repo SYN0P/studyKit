a8a7f8e951e8b656a4c3c6995943a4eaaa3789ea
SYN0P <20095859+SYN0P@users.noreply.github.com>
2022-02-02 15:45:39 +0900

    20220202

---
+++ baekjoon/15651.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+
+        String[] arr = new String[M];
+        StringBuilder sb = new StringBuilder();
+        permutation(sb, arr, N, M, 0, 1);
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+
+    static void permutation(StringBuilder sb, String[] arr, int N, int M, int depth, int before) {
+        for (int i = 1; i <= N; i++) {
+            arr[depth] = Integer.toString(i);
+
+            if (depth == M - 1) sb.append(String.join(" ", arr)).append("\n");
+            else permutation(sb, arr, N, M, depth + 1, i + 1);
+        }
+    }
+}
